b0e5fa0469dd33e9e1dd2441568c3028cbe04b5f
Nate Bruggeman <njbruggeman@gmail.com>
2021-04-25 11:42:50 -0500

    created navigation bar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
     return (
         <header>
-            header
+            <Navbar bg="dark" variant= 'dark' expand="lg" collapseOnSelect>
+                <Navbar.Brand href="/">Unbranded Market</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                    <Nav.Link href="/cart">Cart</Nav.Link>
+                    <Nav.Link href="/login">Sign In</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </header>
     )
 }
