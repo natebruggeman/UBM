893689e8db5df3a19ec970cd59966edce0c105c4
Nate Bruggeman <njbruggeman@gmail.com>
2021-05-08 17:24:35 -0500

    added reviews to featured cards

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const Product = ({ product }) => {
     return (
@@ -7,6 +8,24 @@ const Product = ({ product }) => {
             <a href={`/product/${product._id}`}>
                 <Card.Img src ={product.image} variant='top' />     
             </a>    
+            <Card.Body>
+                <a href={`/product/${product._id}`}>
+                    <Card.Title as='div'> 
+                        <strong>{product.name}</strong>
+                    </Card.Title>
+                </a>    
+
+                <Card.Text>
+                    <Rating 
+                        value={product.rating} 
+                        text= {`${product.numReviews} reviews`}/>
+                </Card.Text>
+
+                <Card.Text as='h3'> ${product.price}
+
+                </Card.Text>
+
+            </Card.Body>  
         </Card>
     )
 }
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,11 +6,11 @@ import products from '../products'
 const HomeScreen = () => {
     return (
         <>
-            <h1>Latest Products</h1>
+            <h1>Featured Styles</h1>
             <Row>
                 {products.map(product =>
                     <Col sm = {12} md = {6} lg = {4} xl = {3}>
-                        <h3>{product.name}</h3>
+                        {/* <h3>{product.name}</h3> */}
                         <Product product={product} />
                     </Col>
                 )}
